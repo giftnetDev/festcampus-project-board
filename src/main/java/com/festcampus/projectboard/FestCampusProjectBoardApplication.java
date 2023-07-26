5fe3866015b98990502ecc330f00d615289f4ca9
giftnet Dev <giftneta@gmail.com>
2023-07-26 16:44:36 +0900

    first commit

---
+++ src/main/java/com/festcampus/projectboard/FestCampusProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package com.festcampus.projectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestCampusProjectBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FestCampusProjectBoardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/festcampus/projectboard/FestcampusProjectBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.festcampus.projectboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestCampusProjectBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
